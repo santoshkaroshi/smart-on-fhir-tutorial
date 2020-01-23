1a91786695589f0ccbadf9c1a86751458533eb25
santoshkaroshi <59604571+santoshkaroshi@users.noreply.github.com>
2020-01-23 15:54:35 +0530

    Update example-smart-app.js

--- example-smart-app/src/js/example-smart-app.js
+++ example-smart-app/src/js/example-smart-app.js
@@ -48,6 +48,7 @@
           p.fname = fname;
           p.lname = lname;
           p.height = getQuantityValueAndUnit(height[0]);
+          p.identifier = patient.identifier;
 
           if (typeof systolicbp != 'undefined')  {
             p.systolicbp = systolicbp;
@@ -83,6 +84,7 @@
       diastolicbp: {value: ''},
       ldl: {value: ''},
       hdl: {value: ''},
+      identifier: {value: ''}
     };
   }
 
@@ -126,6 +128,9 @@
     $('#diastolicbp').html(p.diastolicbp);
     $('#ldl').html(p.ldl);
     $('#hdl').html(p.hdl);
+    $('#identifier').html(p.identifier);
+    
+    
   };
 
 })(window);
