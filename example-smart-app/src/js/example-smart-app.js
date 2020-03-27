70005d423342970ebf7588bf18af89c6b4367b7a
santoshkaroshi <59604571+santoshkaroshi@users.noreply.github.com>
2020-03-27 12:34:52 +0530

    Update example-smart-app.js

--- example-smart-app/src/js/example-smart-app.js
+++ example-smart-app/src/js/example-smart-app.js
@@ -27,7 +27,7 @@
         $.when(pt, obv).done(function(patient, obv) {
           var byCodes = smart.byCodes(obv, 'code');
           var gender = patient.gender;
-          var phone = '';
+          var active = '';
           
           var fname = '';
           var lname = '';             
@@ -49,7 +49,7 @@
           p.fname = fname;
           p.lname = lname;         
           p.height = getQuantityValueAndUnit(height[0]);   
-          p.phone = phone;
+          p.active = patient.active;
         
           if (typeof systolicbp != 'undefined')  {
             p.systolicbp = systolicbp;
@@ -85,7 +85,7 @@
       diastolicbp: {value: ''},
       ldl: {value: ''},
       hdl: {value: ''},
-      phone: {value: ''} 
+      active: {value: ''} 
     };
   }
 
@@ -129,7 +129,7 @@
     $('#diastolicbp').html(p.diastolicbp);
     $('#ldl').html(p.ldl);
     $('#hdl').html(p.hdl);  
-    $('#phone').html(p.phone);  
+    $('#active').html(p.active);  
   };
 
 })(window);
